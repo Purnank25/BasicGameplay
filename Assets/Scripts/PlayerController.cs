7a5f0fa37ecc4ab0a0e1756849eb0af75612fb91
Purnank25 <purnankexcel1@gmail.com>
2025-05-20 16:48:13 +0530

    keeping playing in a particular range

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     public float horizontalInput;
     public float horizontalSpeed = 10.0f;
+    public float xlimit = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,5 +16,13 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * horizontalSpeed);
+        if(transform.position.x < -xlimit)
+        {
+            transform.position = new Vector3(-xlimit,transform.position.y,transform.position.z);
+        }
+        if (transform.position.x > xlimit)
+        {
+            transform.position = new Vector3(xlimit, transform.position.y, transform.position.z);
+        }
     }
 }
