6ef75887819f6c8f7eb2bdba1939dfd1d44fb39c
Purnank25 <purnankexcel1@gmail.com>
2025-05-21 13:02:31 +0530

    adding randomness for animal spwan

--- Assets/Scripts/SpwanManager.cs
+++ Assets/Scripts/SpwanManager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class SpwanManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    public float spwanRangeX = 20;
+    public float spwanRangeZ = 20;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,7 +17,8 @@ public class SpwanManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.S))
         {
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            Vector3 spwanPos = new Vector3(Random.Range(-spwanRangeX, spwanRangeX), 0, spwanRangeZ);
+            Instantiate(animalPrefabs[animalIndex], spwanPos, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
