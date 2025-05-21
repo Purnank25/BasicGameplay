dc1d916189291d5053000e3948083746885a302b
Purnank25 <purnankexcel1@gmail.com>
2025-05-21 15:51:26 +0530

    added collision detection to destroy animals and food

--- Assets/Scripts/DetectCollision.cs
+++ Assets/Scripts/DetectCollision.cs
@@ -15,6 +15,7 @@ public class DetectCollision : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        
+        Destroy(gameObject);
+        Destroy(other.gameObject);
     }
 }
