5768f320fa49c73c316364977fb36fc363b215c2
Purnank25 <purnankexcel1@gmail.com>
2025-05-21 11:30:40 +0530

    adding prefabs of food

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public float horizontalInput;
     public float horizontalSpeed = 10.0f;
     public float xlimit = 10;
+    public GameObject projectilePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
